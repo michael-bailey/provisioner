1d065d49e89319b329ea6e6c94af48897994c9ea
michael-bailey <mickyb18a@gmail.com>
2025-06-18 13:31:01 +0100

    Extracted step logic into a really weird function.
    Not a fan but it should work for now.

--- provisiond/src/provisioner_server.rs
+++ provisiond/src/provisioner_server.rs
@@ -1,5 +1,5 @@
-use std::sync::Arc;
 use std::collections::VecDeque;
+use std::sync::Arc;
 
 use log::info;
 use tonic::{Code, Request, Response, Status};
@@ -9,7 +9,7 @@ use libprovision::hello_world::{
     PullResponse, RestartRequest, RestartResponse,
 };
 
-use crate::executors::RealCreateExecutor;
+use crate::executors::{CreateExecutorError, DeleteExecutorError, RealCreateExecutor};
 use crate::executors::RealDeleteExecutor;
 use crate::executors::{CreateExecutor, DeleteExecutor};
 
@@ -19,16 +19,55 @@ pub struct ProvisionerImpl {
 }
 
 impl ProvisionerImpl {
-
     pub(crate) fn unwind(
         &self,
-        queue: VecDeque<Box<dyn FnOnce() -> () + Send>>,
+        service_name: String,
+        queue: VecDeque<Box<dyn FnOnce(String) -> () + Send>>,
     ) -> Result<(), Status> {
         for f in queue.into_iter().rev() {
-            f();
+            f(service_name.clone());
         }
 
         Ok(())
+    }
+
+    fn run_step(
+        &self,
+        service_name: &String,
+        step_name: &'static str,
+        undo_stack: &mut VecDeque<Box<dyn FnOnce(String) + Send>>,
+        step_fn: &dyn Fn(String) -> Result<(), CreateExecutorError>,
+        inverse_fn: &dyn Fn(String) -> Result<(), DeleteExecutorError>,
+    ) -> Option<Result<Response<CreateResponse>, Status>> {
+
+        info!("Running step '{step_name}' for: {service_name}", step_name = step_name, service_name = service_name);
+
+        let name = service_name.clone();
+        let step_res = step_fn(name);
+
+        if let Err(e) = step_res {
+            info!(
+                "Step '{step_name}' failed unwinding stack for {service_name} got error {err}",
+                step_name=  step_name,
+                service_name = service_name,
+                err = e.to_string()
+            );
+            return Some(Err(Status::new(
+                Code::Internal,
+                format!("Error Running step '{step_name}' for {service_name} got error {err}", step_name = step_name, service_name = service_name,err = e.to_string()),
+
+            )));
+        }
+
+        info!("Appending undo folder for: {}", service_name);
+
+        let delete_executor = self.delete_executor.clone();
+        undo_stack.push_back(Box::new(move |name| {
+            info!("Deleting folder for: {}", name);
+            delete_executor.delete_folder(name).unwrap();
+        }));
+
+        None
     }
 }
 
@@ -54,85 +93,56 @@ impl Provisioner for ProvisionerImpl {
         );
 
         info!("Creating undo queue for: {}", service_name);
-        let mut undo_stack: VecDeque<Box<dyn FnOnce() -> () + Send>> = VecDeque::new();
+        let mut undo_stack: VecDeque<Box<dyn FnOnce(String) -> () + Send>> = VecDeque::new();
 
-
-        info!("Creating folder for: {}", service_name);
-        let name = service_name.clone();
-        let folder_res = self.create_executor.create_folder(name);
-
-        if let Err(e) = folder_res {
-            info!("Folder creation failed unwinding stack for {} got error {}", service_name, e.to_string());
-            let _ = self.unwind(undo_stack);
-            return Err(Status::new(Code::Internal, "Error creating service folder"));
+        // Create folder step
+        if let Some(value) = self.run_step(
+            &service_name,
+            "Create Folder",
+            &mut undo_stack,
+            & |n| { self.create_executor.create_folder(n) },
+            & |n| { self.delete_executor.delete_folder(n) }
+        ) {
+            self.unwind(service_name, undo_stack)?;
+            return value;
         }
 
-        info!("Appending undo folder for: {}", service_name);
-        let name = service_name.clone();
-        let delete_executor = self.delete_executor.clone();
-        undo_stack.push_back(Box::new(move || {
-            info!("Deleting folder for: {}", name);
-            delete_executor.delete_folder(name).unwrap();
-        }));
-
-
-        info!("Creating compose file for: {}", service_name);
-        let name = service_name.clone();
-        let compose_file_res = self.create_executor.create_compose_file(name);
-
-        if let Err(e) = compose_file_res {
-            info!("Compose file creation failed unwinding stack for {} got error {}", service_name, e.to_string());
-            let _ = self.unwind(undo_stack);
-            return Err(Status::new(Code::Internal, "Error creating compose file"));
+        // Create compose file step
+        if let Some(value) = self.run_step(
+            &service_name,
+            "Create Compose File",
+            &mut undo_stack,
+            & |n| { self.create_executor.create_compose_file(n) },
+            & |n| { self.delete_executor.delete_compose_file(n) }
+        ) {
+            self.unwind(service_name, undo_stack)?;
+            return value;
         }
 
-        info!("Appending undo compose file for: {}", service_name);
-        let name = service_name.clone();
-        let delete_executor = self.delete_executor.clone();
-        undo_stack.push_back(Box::new(move || {
-            info!("Deleting compose file for: {}", name);
-            delete_executor.delete_compose_file(name).unwrap();
-        }));
-
-
-        info!("Creating env file for: {}", service_name);
-        let name = service_name.clone();
-        let env_file_res = self.create_executor.create_env_file(name);
-
-        if let Err(e) = env_file_res {
-            info!("Env file creation failed unwinding stack for {} got error {}", service_name, e.to_string());
-            let _ = self.unwind(undo_stack);
-            return Err(Status::new(Code::Internal, "Error creating compose file"));
+        // Create env file step
+        if let Some(value) = self.run_step(
+            &service_name,
+            "Create Compose File",
+            &mut undo_stack,
+            & |n| { self.create_executor.create_env_file(n) },
+            & |n| { self.delete_executor.delete_env_file(n) }
+        ) {
+            self.unwind(service_name, undo_stack)?;
+            return value;
         }
 
-        info!("Appending undo env file for: {}", service_name);
-        let name = service_name.clone();
-        let delete_executor = self.delete_executor.clone();
-        undo_stack.push_back(Box::new(move || {
-            info!("Deleting env file for: {}", name);
-            delete_executor.delete_env_file(name).unwrap();
-        }));
-
-
-        info!("Creating unit file for: {}", service_name);
-        let name = service_name.clone();
-        let unit_file_res = self.create_executor.create_systemd_unit(name);
-
-        if let Err(e) = unit_file_res {
-            info!("Unit file creation failed unwinding stack for {} got error {}", service_name, e.to_string());
-            let _ = self.unwind(undo_stack);
-            return Err(Status::new(Code::Internal, "Error creating compose file"));
+        // Create unit file step
+        if let Some(value) = self.run_step(
+            &service_name,
+            "Create Compose File",
+            &mut undo_stack,
+            & |n| { self.create_executor.create_systemd_unit(n) },
+            & |n| { self.delete_executor.delete_systemd_unit(n) }
+        ) {
+            self.unwind(service_name, undo_stack)?;
+            return value;
         }
-
-        info!("Appending undo unit file for: {}", service_name);
-        let name = service_name.clone();
-        let delete_executor = self.delete_executor.clone();
-        undo_stack.push_back(Box::new(move || {
-            info!("deleting unit file for: {}", name);
-            delete_executor.delete_systemd_unit(name).unwrap();
-        }));
-
-
+        
         info!("Created service folder for: {}", service_name);
         Ok(Response::new(CreateResponse {}))
     }
